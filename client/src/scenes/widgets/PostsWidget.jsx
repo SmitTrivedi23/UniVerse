82ce27a82e5360286c7106d30b58b3061c0a545f
Smit <smittrivedi23@gmail.com>
2023-08-06 18:47:13 +0530

    Updated PostsWidget sorting posts by Date

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -1,4 +1,82 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { setPosts } from "state";
+// import PostWidget from "./PostWidget";
+
+// const PostsWidget = ({ userId, isProfile = false }) => {
+//   const dispatch = useDispatch();
+//   const posts = useSelector((state) => state.posts);
+//   const token = useSelector((state) => state.token);
+
+//   const getPosts = async () => {
+//     const response = await fetch("https://universe-nmi5.onrender.com/posts", {
+//       method: "GET",
+//       headers: { Authorization: `Bearer ${token}` },
+//     });
+//     const data = await response.json();
+//     dispatch(setPosts({ posts: data }));
+    
+//   };
+
+//   const getUserPosts = async () => {
+//     const response = await fetch(
+//       `https://universe-nmi5.onrender.com/posts/${userId}/posts`,
+//       {
+//         method: "GET",
+//         headers: { Authorization: `Bearer ${token}` },
+//       }
+//     );
+//     const data = await response.json();
+//     dispatch(setPosts({ posts: data }));
+//   };
+
+//   useEffect(() => {
+//     if (isProfile) {
+//       getUserPosts();
+//     } else {
+//       getPosts();
+//     }
+//   }, []); //eslint-disable-line react-hooks/exhaustive-deps
+//   console.log(posts);
+//   return (
+//     <>
+//       {posts.map(
+//         ({
+//           _id,
+//           userId,
+//           firstName,
+//           lastName,
+//           description,
+//           location,
+//           picturePath,
+//           userPicturePath,
+//           likes,
+//           comments,
+//         }) => (
+//           <PostWidget
+//             key={_id}
+//             postId={_id}
+//             postUserId={userId}
+//             name={`${firstName} ${lastName}`}
+//             description={description}
+//             location={location}
+//             picturePath={picturePath}
+//             userPicturePath={userPicturePath}
+//             likes={likes}
+//             comments={comments}
+//           />
+//         )
+//       )}
+//     </>
+//   );
+// };
+
+// export default PostsWidget;
+
+
+//Sorting post by date
+
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "state";
 import PostWidget from "./PostWidget";
@@ -8,8 +86,13 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const posts = useSelector((state) => state.posts);
   const token = useSelector((state) => state.token);
 
+  const sortPostsByDate = (posts) => {
+    const sortedPosts = [...posts]; // Create a new array to avoid modifying the original array
+    return sortedPosts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+    };
+
   const getPosts = async () => {
-    const response = await fetch("http://localhost:3001/posts", {
+    const response = await fetch("https://universe-nmi5.onrender.com/posts", {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
@@ -19,7 +102,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
   const getUserPosts = async () => {
     const response = await fetch(
-      `http://localhost:3001/posts/${userId}/posts`,
+      `https://universe-nmi5.onrender.com/posts/${userId}/posts`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
@@ -37,9 +120,12 @@ const PostsWidget = ({ userId, isProfile = false }) => {
     }
   }, []); //eslint-disable-line react-hooks/exhaustive-deps
 
+  // Sort the posts by date
+  const sortedPosts = sortPostsByDate(posts);
+
   return (
     <>
-      {posts.map(
+      {sortedPosts.map(
         ({
           _id,
           userId,
